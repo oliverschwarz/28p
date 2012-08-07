4a3731d24fbc3805c170d1a5878f17e2aeb41e4c
Oliver Schwarz <oliver.schwarz@gmail.com>
2012-08-07 13:59:43 +0200

    Adding Google Analytics and corrected some style issues.

--- functions.php
+++ functions.php
@@ -256,6 +256,46 @@ html;
     
 }
 
+if (!function_exists('pistole_custom_header')) {
+    
+    /**
+     * Custom header
+     * 
+     * Sets up custom header components to be hooked into
+     * the wp_head() function.
+     * 
+     * @return void
+     */
+    function pistole_custom_header()
+    {
+
+        $output = '';
+        
+        if (!current_user_can('level_10')) {
+            $output .= <<<html
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);
+_gaq.push (['_gat._anonymizeIp']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+html;
+        }
+        
+        echo $output;
+        
+    }
+    
+    // add custom header
+    add_action('wp_head', 'pistole_custom_header');
+    
+}
+
 // Theme footer libraries
 if (!function_exists('pistole_custom_footer')) {
 
